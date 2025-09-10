dcc8457070324dd1c06073bce35d130876fb6ba5
Masonlin2104 <mason.lin2004@gmail.com>
2025-09-10 11:15:02 -0400

    Clickable points added, Rewrote code as a reuseable function

--- main.py
+++ main.py
@@ -1,43 +1,62 @@
 import cv2 as cv
 import numpy as np
 
+def findClickablePoints(target_image_path, map_image_path, threshold=0.56):
 #Load images
-cabbage_patch = cv.imread("cabbage_patch.png", cv.IMREAD_UNCHANGED)
-cabbage = cv.imread("cabbage.png", cv.IMREAD_UNCHANGED)
+    map_image = cv.imread(map_image_path, cv.IMREAD_UNCHANGED)
+    target_image = cv.imread(target_image_path, cv.IMREAD_UNCHANGED)
 
-if cabbage_patch is None or cabbage is None:
-    print("Error: Could not load images")
-    exit()
+    if map_image is None or target_image is None:
+     print("Error: Could not load images")
+     exit()
 
 #Template matching
-result = cv.matchTemplate(cabbage_patch, cabbage, cv.TM_CCOEFF_NORMED)
+    result = cv.matchTemplate(map_image, target_image, cv.TM_CCOEFF_NORMED)
 
 #Threshold for declaring a match
-threshold = 0.56
-locations = np.where(result >= threshold)
+    locations = np.where(result >= threshold)
 
 #Get template size
-h, w = cabbage.shape[:2]
+    h, w = target_image.shape[:2]
 
-rectangles = []
+    rectangles = []
+    points = []
 
 #Loop through all matches, add to rectangles array
-for pt in zip(*locations[::-1]):  # (x, y) points
-    top_left = pt
-    bottom_right = (pt[0] + w, pt[1] + h)
-    rect = [pt[0], pt[1], w, h]
-    rectangles.append(rect)
-    rectangles.append(rect) #append twice for cases where there is only 1 rect on object
+    for pt in zip(*locations[::-1]):  # (x, y) points
+     top_left = pt
+     bottom_right = (pt[0] + w, pt[1] + h)
+     rect = [pt[0], pt[1], w, h]
+     rectangles.append(rect)
+     rectangles.append(rect) #append twice for cases where there is only 1 rect on object
 
 #Group rectangles together
-group_rect, rect_weights = cv.groupRectangles(rectangles, 1)
+    group_rect, rect_weights = cv.groupRectangles(rectangles, 1)
+
+    line_color = (0,255,0)
+    line_type = cv.LINE_4
+    marker_color = (255, 0, 255)
+    marker_type = cv.MARKER_CROSS
+
 
 #Print rectangles
-for (x, y, w, h) in group_rect:
-    cv.rectangle(cabbage_patch, (x,y), (x + w, y + h), (0,0,255), 2)
+    for (x, y, w, h) in group_rect:
 
+     center_x = x + int(w/2)
+     center_y = y + int(h/2)
 
+     points.append((center_x, center_y))
+     cv.drawMarker(map_image, (center_x, center_y), marker_color, marker_type)
+     cv.rectangle(map_image, (x,y), (x + w, y + h), (0,0,255), 2)
 
+    return points, map_image
+
+points, after_image = findClickablePoints("cabbage.png", "cabbage_patch.png")
+print(points)
+
+'''
 #Show result
-cv.imshow("Detected Cabbages", cabbage_patch)
+cv.imshow("Detected Cabbages", after_image)
 cv.waitKey(0)
+'''
+
